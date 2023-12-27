f033d4630bb2403e2da08eb3269bb11bfc48c013
MUCHIRA JUNIOR <muchirajunior022@gmail.com>
2023-12-27 22:46:32 +0300

    update signals :gear:

--- src/components/todos/todos.jsx
+++ src/components/todos/todos.jsx
@@ -14,9 +14,13 @@ export default function Todos() {
 
   function updateTodo(task) {
     console.log(task);
+    task.completed = !task.completed;
     todos.value=todos.value.map((item) => item.title === task.title ? task : item);
   }
 
+  function deleteTodo(todo){
+    todos.value= todos.value.filter((td)=>td !==todo )
+  }
   return (
     <div className='container pt-3'>
       <div className="d-flex flex-row">
@@ -26,10 +30,12 @@ export default function Todos() {
       <h3 className="mt-5">Todos</h3>
       <div className="card m-2 p-3">
         {todos.value.map((todo)=>
-          <div className="mb-3 form-check" key={todo.title}>
-            <input type="checkbox" className="form-check-input" checked={todo.completed} onChange={(_) => updateTodo({ ...todo, completed: !todo.completed })} />
+          <div className="mb-3 form-check d-flex flex-row" key={todo.title}>
+            <input type="checkbox" className="form-check-input me-2" checked={todo.completed} onChange={(_) => updateTodo(todo)} />
             <label className="form-check-label" htmlFor="exampleCheck1">{todo.title}</label>
-            <button className="btn btn-info btn-sm float-end">update</button>
+            <span className="flex-fill"></span>
+            <button className="btn btn-info btn-sm me-2">update</button>
+            <button className="btn btn-danger btn-sm " onClick={()=>deleteTodo(todo)} >delete</button>
           </div>
         )}
       </div>
